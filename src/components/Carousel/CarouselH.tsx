0b752d696ae6a902092c3d8638b03fe320f98ce6
Agustina <akuocampo@gmail.com>
2023-11-22 15:31:49 -0300

    prueba2 de imagenes

--- src/components/Carousel/CarouselH.tsx
+++ src/components/Carousel/CarouselH.tsx
@@ -1,11 +1,12 @@
 import { Carousel } from "react-bootstrap"
+import c1 from "../../assets/images/c1.jpg"
 
 
 const CarouselH = () => {
   return (
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
-        <img className="d-block w-100" style={{ maxHeight: "500px", objectFit: 'cover' }} src="../../src/assets/images/c1.jpg" alt="" />
+        <img className="d-block w-100" style={{ maxHeight: "500px", objectFit: 'cover' }} src={c1} alt="imagen-carrusel1" />
       </Carousel.Item>
 
       <Carousel.Item>
