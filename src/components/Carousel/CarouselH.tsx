df139385791c99d885c3211556c0f0bcc7731d97
Agustina <akuocampo@gmail.com>
2023-11-22 15:28:11 -0300

    imagenes carrusel

--- src/components/Carousel/CarouselH.tsx
+++ src/components/Carousel/CarouselH.tsx
@@ -5,7 +5,7 @@ const CarouselH = () => {
   return (
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
-        <img className="d-block w-100" style={{ maxHeight: "500px", objectFit: 'cover' }} src="src/assets/images/c1.jpg" alt="" />
+        <img className="d-block w-100" style={{ maxHeight: "500px", objectFit: 'cover' }} src="../../src/assets/images/c1.jpg" alt="" />
       </Carousel.Item>
 
       <Carousel.Item>
